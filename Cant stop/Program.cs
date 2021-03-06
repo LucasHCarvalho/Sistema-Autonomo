add1bb001d3be8d18efd79958c3280fd617b8493
Lucas Henrique <lcarvalho.henrique@gmail.com>
2021-03-05 23:48:22 -0300

    Mostra Jogadores e formulário Tabuleiro

--- Cant stop/Program.cs
+++ Cant stop/Program.cs
@@ -16,7 +16,7 @@ namespace Cant_stop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Tabuleiro());
         }
     }
 }
