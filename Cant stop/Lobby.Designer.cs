8c46dee6be2cfb6ef9c1a8cdd298767b7cd3c5b2
dmcshiki <dmcshiki@gmail.com>
2021-03-12 16:13:38 -0300

    Feito o conteudo de adicionar partida, entrar na partida e listar os jogadores da partida especifica

--- Cant stop/Lobby.Designer.cs
+++ Cant stop/Lobby.Designer.cs
@@ -32,13 +32,26 @@ namespace Cant_stop
             this.btnMostraLista = new System.Windows.Forms.Button();
             this.lstListarPartidas = new System.Windows.Forms.ListBox();
             this.btnFechar = new System.Windows.Forms.Button();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.btnCriar = new System.Windows.Forms.Button();
+            this.btnListaJogadores = new System.Windows.Forms.Button();
+            this.txtCriarPartida = new System.Windows.Forms.TextBox();
+            this.lblCriarPartida = new System.Windows.Forms.Label();
+            this.lblSenhaPartida = new System.Windows.Forms.Label();
+            this.txtSenhaPartida = new System.Windows.Forms.TextBox();
+            this.lblNomeJogador = new System.Windows.Forms.Label();
+            this.txtNomeJogador = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.lstListaJogadores = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // btnMostraLista
             // 
-            this.btnMostraLista.Location = new System.Drawing.Point(12, 12);
+            this.btnMostraLista.Location = new System.Drawing.Point(16, 15);
+            this.btnMostraLista.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnMostraLista.Name = "btnMostraLista";
-            this.btnMostraLista.Size = new System.Drawing.Size(128, 51);
+            this.btnMostraLista.Size = new System.Drawing.Size(171, 63);
             this.btnMostraLista.TabIndex = 0;
             this.btnMostraLista.Text = "Mostra Lista";
             this.btnMostraLista.UseVisualStyleBackColor = true;
@@ -47,34 +60,164 @@ namespace Cant_stop
             // lstListarPartidas
             // 
             this.lstListarPartidas.BackColor = System.Drawing.SystemColors.Window;
-            this.lstListarPartidas.Location = new System.Drawing.Point(240, 12);
+            this.lstListarPartidas.ItemHeight = 16;
+            this.lstListarPartidas.Location = new System.Drawing.Point(344, 6);
+            this.lstListarPartidas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lstListarPartidas.Name = "lstListarPartidas";
-            this.lstListarPartidas.Size = new System.Drawing.Size(177, 199);
+            this.lstListarPartidas.Size = new System.Drawing.Size(235, 244);
             this.lstListarPartidas.TabIndex = 1;
+            this.lstListarPartidas.SelectedIndexChanged += new System.EventHandler(this.lstListarPartidas_SelectedIndexChanged);
             // 
             // btnFechar
             // 
             this.btnFechar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btnFechar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnFechar.Location = new System.Drawing.Point(12, 160);
+            this.btnFechar.Location = new System.Drawing.Point(16, 101);
+            this.btnFechar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnFechar.Name = "btnFechar";
-            this.btnFechar.Size = new System.Drawing.Size(128, 51);
+            this.btnFechar.Size = new System.Drawing.Size(171, 63);
             this.btnFechar.TabIndex = 2;
             this.btnFechar.Text = "Fechar";
             this.btnFechar.UseVisualStyleBackColor = false;
             this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
             // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(369, 429);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(171, 57);
+            this.btnEntrar.TabIndex = 3;
+            this.btnEntrar.Text = "Entrar na Partida";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCriar
+            // 
+            this.btnCriar.Location = new System.Drawing.Point(755, 200);
+            this.btnCriar.Name = "btnCriar";
+            this.btnCriar.Size = new System.Drawing.Size(171, 60);
+            this.btnCriar.TabIndex = 4;
+            this.btnCriar.Text = "Criar Partida";
+            this.btnCriar.UseVisualStyleBackColor = true;
+            this.btnCriar.Click += new System.EventHandler(this.btnCriar_Click);
+            // 
+            // btnListaJogadores
+            // 
+            this.btnListaJogadores.Location = new System.Drawing.Point(648, 334);
+            this.btnListaJogadores.Name = "btnListaJogadores";
+            this.btnListaJogadores.Size = new System.Drawing.Size(111, 60);
+            this.btnListaJogadores.TabIndex = 5;
+            this.btnListaJogadores.Text = "Listar Jogadores";
+            this.btnListaJogadores.UseVisualStyleBackColor = true;
+            this.btnListaJogadores.Click += new System.EventHandler(this.btnListaJogadores_Click);
+            // 
+            // txtCriarPartida
+            // 
+            this.txtCriarPartida.Location = new System.Drawing.Point(719, 43);
+            this.txtCriarPartida.Name = "txtCriarPartida";
+            this.txtCriarPartida.Size = new System.Drawing.Size(253, 22);
+            this.txtCriarPartida.TabIndex = 6;
+            this.txtCriarPartida.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblCriarPartida
+            // 
+            this.lblCriarPartida.AutoSize = true;
+            this.lblCriarPartida.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCriarPartida.Location = new System.Drawing.Point(734, 15);
+            this.lblCriarPartida.Name = "lblCriarPartida";
+            this.lblCriarPartida.Size = new System.Drawing.Size(222, 25);
+            this.lblCriarPartida.TabIndex = 7;
+            this.lblCriarPartida.Text = "Digite o nome da partida";
+            // 
+            // lblSenhaPartida
+            // 
+            this.lblSenhaPartida.AutoSize = true;
+            this.lblSenhaPartida.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblSenhaPartida.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenhaPartida.Location = new System.Drawing.Point(735, 99);
+            this.lblSenhaPartida.Name = "lblSenhaPartida";
+            this.lblSenhaPartida.Size = new System.Drawing.Size(216, 24);
+            this.lblSenhaPartida.TabIndex = 8;
+            this.lblSenhaPartida.Text = "Digite a senha da partida";
+            // 
+            // txtSenhaPartida
+            // 
+            this.txtSenhaPartida.Location = new System.Drawing.Point(719, 142);
+            this.txtSenhaPartida.Name = "txtSenhaPartida";
+            this.txtSenhaPartida.Size = new System.Drawing.Size(253, 22);
+            this.txtSenhaPartida.TabIndex = 9;
+            this.txtSenhaPartida.TextChanged += new System.EventHandler(this.txtSenhaPartida_TextChanged);
+            // 
+            // lblNomeJogador
+            // 
+            this.lblNomeJogador.AutoSize = true;
+            this.lblNomeJogador.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeJogador.Location = new System.Drawing.Point(384, 278);
+            this.lblNomeJogador.Name = "lblNomeJogador";
+            this.lblNomeJogador.Size = new System.Drawing.Size(147, 24);
+            this.lblNomeJogador.TabIndex = 10;
+            this.lblNomeJogador.Text = "Digite seu nome";
+            // 
+            // txtNomeJogador
+            // 
+            this.txtNomeJogador.Location = new System.Drawing.Point(354, 315);
+            this.txtNomeJogador.Name = "txtNomeJogador";
+            this.txtNomeJogador.Size = new System.Drawing.Size(209, 22);
+            this.txtNomeJogador.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(371, 354);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 18);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Digite a senha da partida";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(354, 386);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(209, 22);
+            this.txtSenha.TabIndex = 13;
+            // 
+            // lstListaJogadores
+            // 
+            this.lstListaJogadores.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstListaJogadores.FormattingEnabled = true;
+            this.lstListaJogadores.ItemHeight = 16;
+            this.lstListaJogadores.Location = new System.Drawing.Point(765, 334);
+            this.lstListaJogadores.Name = "lstListaJogadores";
+            this.lstListaJogadores.Size = new System.Drawing.Size(225, 100);
+            this.lstListaJogadores.TabIndex = 14;
+            this.lstListaJogadores.SelectedIndexChanged += new System.EventHandler(this.lstListaJogadores_SelectedIndexChanged);
+            // 
             // Lobby
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(528, 348);
+            this.ClientSize = new System.Drawing.Size(1022, 498);
+            this.Controls.Add(this.lstListaJogadores);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNomeJogador);
+            this.Controls.Add(this.lblNomeJogador);
+            this.Controls.Add(this.txtSenhaPartida);
+            this.Controls.Add(this.lblSenhaPartida);
+            this.Controls.Add(this.lblCriarPartida);
+            this.Controls.Add(this.txtCriarPartida);
+            this.Controls.Add(this.btnListaJogadores);
+            this.Controls.Add(this.btnCriar);
+            this.Controls.Add(this.btnEntrar);
             this.Controls.Add(this.btnFechar);
             this.Controls.Add(this.lstListarPartidas);
             this.Controls.Add(this.btnMostraLista);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Lobby";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,6 +226,18 @@ namespace Cant_stop
         private System.Windows.Forms.Button btnMostraLista;
         private System.Windows.Forms.ListBox lstListarPartidas;
         private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Button btnCriar;
+        private System.Windows.Forms.Button btnListaJogadores;
+        private System.Windows.Forms.TextBox txtCriarPartida;
+        private System.Windows.Forms.Label lblCriarPartida;
+        private System.Windows.Forms.Label lblSenhaPartida;
+        private System.Windows.Forms.TextBox txtSenhaPartida;
+        private System.Windows.Forms.Label lblNomeJogador;
+        private System.Windows.Forms.TextBox txtNomeJogador;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.ListBox lstListaJogadores;
     }
 }
 
--- Cant stop/Lobby.cs
+++ Cant stop/Lobby.cs
@@ -23,7 +23,6 @@ namespace Cant_stop
         private void btnMostraLista_Click(object sender, EventArgs e)
         {
             string retorno = Jogo.ListarPartidas("T");
-
             retorno = retorno.Replace("\r", "");
             string[] linha = retorno.Split('\n');
 
@@ -40,6 +39,58 @@ namespace Cant_stop
             string[] itens = linha.Split(',');
             this.IdPartida = Convert.ToInt32(itens[0]);
             this.Close();
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string linha = lstListarPartidas.SelectedItem.ToString();
+            string[] itens = linha.Split(',');
+            this.IdPartida = Convert.ToInt32(itens[0]);
+            string jogador = Jogo.EntrarPartida(this.IdPartida, txtNomeJogador.Text, txtSenha.Text);   
+        }
+
+        private void btnCriar_Click(object sender, EventArgs e)
+        {
+           string criarPartida = Jogo.CriarPartida(txtCriarPartida.Text, txtSenhaPartida.Text);
+        }
+
+        private void btnListaJogadores_Click(object sender, EventArgs e)
+        {
+            string linha = lstListarPartidas.SelectedItem.ToString();
+            string[] itens = linha.Split(',');
+            this.IdPartida = Convert.ToInt32(itens[0]);
+
+            string listajogadores = Jogo.ListarJogadores(this.IdPartida);
+            listajogadores = listajogadores.Replace("\r", "");
+            string[] espaco = listajogadores.Split('\n');
+
+            for (int i = 0; i < espaco.Length; i++)
+            {
+                lstListaJogadores.Items.Add(espaco[i]);
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSenhaPartida_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstListarPartidas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstListaJogadores_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
