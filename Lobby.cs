cabffc2e930435703fdd3cf0e7f47f0a85055212
Lorenzo1507 <61765932+Lorenzo1507@users.noreply.github.com>
2021-04-05 16:18:10 -0300

    Tabuleiro

---
+++ Lobby.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CantStopServer;
+
+namespace Cant_stop
+{
+    public partial class Lobby : Form
+    {        
+        public int IdPartida{ get; set; }
+        public int IdJogador { get; set; }
+        public String senhaJogador { get; set; }
+        public Lobby()
+        {
+            InitializeComponent();
+            lblVersao.Text = "Versão " + Jogo.Versao;
+        }
+       
+        private void btnMostraLista_Click(object sender, EventArgs e)
+        {
+            string retorno = Jogo.ListarPartidas("T");
+            retorno = retorno.Replace("\r", "");
+            string[] linha = retorno.Split('\n');
+
+            lstListarPartidas.Items.Clear();
+            for (int i = 0; i < linha.Length; i++)
+            {
+                lstListarPartidas.Items.Add(linha[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string linha = lstListarPartidas.SelectedItem.ToString();
+            string[] itens = linha.Split(',');
+            this.IdPartida = Convert.ToInt32(itens[0]);
+            string entrar = Jogo.EntrarPartida(this.IdPartida, txtNomeJogador.Text, txtSenha.Text);
+            lblTratativaErro.Text = entrar;
+    }
+
+        private void btnCriar_Click(object sender, EventArgs e)
+        {
+           string criarPartida = Jogo.CriarPartida(txtCriarPartida.Text, txtSenhaPartida.Text);
+        }
+
+        private void btnListaJogadores_Click(object sender, EventArgs e)
+        {
+            string linha = lstListarPartidas.SelectedItem.ToString();
+            string[] itens = linha.Split(',');
+            this.IdPartida = Convert.ToInt32(itens[0]);
+            
+            string listajogadores = Jogo.ListarJogadores(this.IdPartida);
+            /*listajogadores = listajogadores.Replace("\r", "");*/
+            string[] espaco = listajogadores.Split('\n');
+            lstListaJogadores.Items.Clear();
+            for (int i = 0; i < espaco.Length; i++)
+            {
+                lstListaJogadores.Items.Add(espaco[i]);
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSenhaPartida_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstListarPartidas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstListaJogadores_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblversao_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Lobby_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string lista = lblTratativaErro.Text.ToString();
+            string[] itens = lista.Split(',');
+            string res = lblTratativaErro.Text.ToString().Substring(0, 1);
+            if(res == "E")
+            {
+                MessageBox.Show("Erro: Senha de acesso para iniciar partida incorreto", "Senha incorreta",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else { 
+            this.IdJogador = Convert.ToInt32(itens[0]);
+            this.senhaJogador = itens[1];
+            MessageBox.Show(this.senhaJogador);
+            string partida = Jogo.IniciarPartida(IdJogador, senhaJogador);
+            inGame ingame = new inGame();
+            ingame.ShowDialog();
+            }
+        }
+
+        private void lblTratativaErro_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ inGame.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace Cant_stop
+{
+    partial class inGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(inGame));
+            this.lstJogadores = new System.Windows.Forms.ListBox();
+            this.btnPartidasJ = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstJogadores
+            // 
+            this.lstJogadores.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstJogadores.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstJogadores.FormattingEnabled = true;
+            this.lstJogadores.ItemHeight = 22;
+            this.lstJogadores.Location = new System.Drawing.Point(167, 12);
+            this.lstJogadores.Name = "lstJogadores";
+            this.lstJogadores.Size = new System.Drawing.Size(186, 176);
+            this.lstJogadores.TabIndex = 0;
+            this.lstJogadores.SelectedIndexChanged += new System.EventHandler(this.lstJogadores_SelectedIndexChanged);
+            // 
+            // btnPartidasJ
+            // 
+            this.btnPartidasJ.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPartidasJ.Location = new System.Drawing.Point(12, 12);
+            this.btnPartidasJ.Name = "btnPartidasJ";
+            this.btnPartidasJ.Size = new System.Drawing.Size(128, 96);
+            this.btnPartidasJ.TabIndex = 1;
+            this.btnPartidasJ.Text = "Mostrar Lista de Partidas";
+            this.btnPartidasJ.UseVisualStyleBackColor = true;
+            this.btnPartidasJ.Click += new System.EventHandler(this.btnPartidasJ_Click);
+            // 
+            // inGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPartidasJ);
+            this.Controls.Add(this.lstJogadores);
+            this.DoubleBuffered = true;
+            this.Name = "inGame";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstJogadores;
+        private System.Windows.Forms.Button btnPartidasJ;
+    }
+}
